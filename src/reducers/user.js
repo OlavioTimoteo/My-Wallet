42a3ff073848929b92e9c30ef816e6e2c1fa045f
Olávio Timóteo <olaviotimoteo@hotmail.com>
2022-01-25 12:19:26 -0300

    Adiciona actions, reducers e a store ao projeto

---
+++ src/reducers/user.js
@@ -0,0 +1,14 @@
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default userReducer;

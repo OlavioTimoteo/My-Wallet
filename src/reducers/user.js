6e8e38c43cafca97e29c85edb684c0e3ebf555ee
Olávio Timóteo <olaviotimoteo@hotmail.com>
2022-02-28 16:52:52 -0300

    Atualiza actions, reducers e store

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,3 +1,4 @@
+// Esse reducer será responsável por tratar as informações da pessoa usuária
 import { SAVE_EMAIL } from '../actions';
 
 const INITIAL_STATE = {
@@ -6,14 +7,14 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case SAVE_EMAIL:
-      return {
-        ...state,
-        email: action.payload,
-      };
+  case SAVE_EMAIL:
+    return {
+      ...state,
+      email: action.payload,
+    };
 
-    default:
-      return state;
+  default:
+    return state;
   }
 };
 
